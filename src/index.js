fae16060512949fe92e117938f6970ab4bb5bf23
Emiza Support <emizasupport@emizainc.com>
2022-08-12 16:32:50 +0530

    amplify sso with login page

--- src/index.js
+++ src/index.js
@@ -6,15 +6,61 @@ import reportWebVitals from './reportWebVitals';
 
 import {Amplify} from "aws-amplify";
 import awsExports from "./aws-exports";
+import { UserContextProvider } from './contexts/UserContext'
 
+//Check if you are in localhost or production
+
+const isLocalhost = Boolean(
+
+  window.location.hostname === 'localhost' ||
+
+    // [::1] is the IPv6 localhost address.
+
+    window.location.hostname === '[::1]' ||
+
+    // 127.0.0.0/8 are considered localhost for IPv4.
+
+    window.location.hostname.match(
+
+      /^127(?:.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+
+    )
+
+)
+
+const signInURI = awsExports.oauth.redirectSignIn.split(',')
+
+const signOutURI = awsExports.oauth.redirectSignOut.split(',')
+
+if (isLocalhost) {
+
+  awsExports.oauth.redirectSignIn = signInURI[0]
+
+  awsExports.oauth.redirectSignOut = signOutURI[0]
+
+} else if (window.location.hostname === 'https://main.d211rykl745cll.amplifyapp.com/'){
+
+  awsExports.oauth.redirectSignIn = signInURI[1]
+
+  awsExports.oauth.redirectSignOut = signOutURI[1]
+
+} else {
+
+  console.alert('This is not possible')
+
+}
+
+//Check if you are in localhost or production
+
+//Then Configure Resources
 Amplify.configure(awsExports);
 
 ReactDOM.render(
 
   <React.StrictMode>
-
-    <App />
-
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </React.StrictMode>,
 
   document.getElementById('root')
